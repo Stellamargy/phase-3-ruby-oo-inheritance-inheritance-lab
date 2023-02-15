40ba2a1581ef6a1dc6f3e17114b4fbbea3477b8c
Stellamargy <stella.oloo@student.moringaschool.com>
2023-02-15 15:30:14 +0300

    passed all test

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+    def initialize(knowledge=[])  
+        @knowledge=knowledge
+    end
+    
+    def learn(string_of_knowledge)
+        @knowledge << string_of_knowledge.to_s
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
